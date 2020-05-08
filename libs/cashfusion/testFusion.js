4f73c059624db63c944d04d4879d78ba0adcf10d
Shomari Prince <nyusternie@sdot.io>
2020-05-07 23:44:40 -0400

    Fusion test cleanup.

--- libs/cashfusion/testFusion.js
+++ libs/cashfusion/testFusion.js
@@ -13,7 +13,6 @@ const protobuf = require('protobufjs')
 /* Initialize shuffle. */
 // const shuffleIt = repl.start('cashfusion > ')
 
-// const GENESIS = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
 const GENESIS = '6fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000'
 const HOST = 'cashfusion.electroncash.dk'
 const PORT = 8787
@@ -50,70 +49,10 @@ for (let oneTypeName of pbTypes) {
 
 
 /**
- * Send Message
- */
-const sendMessage = function (_msg) {
-    debug('Send message', _msg)
-
-    /* Initialize message length. */
-    const messageLength = Buffer.allocUnsafe(4)
-
-    /* Set message (buffer). */
-    // const message = Buffer.from(_msg.toString())
-    // const message = _msg
-
-    /* Calculate message length. */
-    const lengthDec = _msg.length
-    // const lengthHex = lengthDec.toString(16)
-    // const lengthBuf = Buffer.from(lengthHex, 'hex')
-    // debug('Message length (dec / hex / buf)',
-    //     lengthDec, lengthHex, lengthBuf)
-
-    /* Set message length. */
-    // NOTE: 4-bytes in big-endian (bytes order).
-    // lengthBuf.copy(messageLength)
-    // messageLength.writeUIntBE(lengthBuf, 0, 4)
-    messageLength.writeUIntBE(lengthDec, 0, 4)
-    debug('Message length', messageLength)
-
-    /* Build packet. */
-    const packet = Buffer.concat([
-        magic,
-        messageLength,
-        _msg
-    ])
-    debug('Packet message:', packet)
-    debug('Packet message (string):', packet.toString())
-
-    return packet
-}
-
-/**
  * Greeting
  */
 const greet = function () {
-    debug('Greeting server...')
-
-    // packet = self.packets.packet.add()
-    // packet.packet.from_key.key = verification_key
-    // packet.packet.registration.amount = amount
-    // packet.packet.registration.type = typ
-    // packet.packet.registration.version = version
-
-    // send(pb.ClientHello(version=VERSION, genesis_hash=GENESIS))
-
-    /* Set message. */
-    // const message = PB.Signed.create({
-    //     packet: PB.Packet.create({
-    //         fromKey: PB.VerificationKey.create({ key }),
-    //         registration: PB.Registration.create({
-    //             amount: amount,
-    //             version: protocolVersion,
-    //             // type: 'DEFAULT' // WHY AREN'T WE USING THIS??
-    //             type: 'DEFAULT'
-    //         })
-    //     })
-    // })
+    console.info('\n\tGreeting CashFusion server...\n') // eslint-disable-line no-console
 
     /* Set message. */
     const message = PB.ClientMessage.create({
@@ -123,49 +62,22 @@ const greet = function () {
         })
     })
 
-    // const obj = {
-    //     version: Buffer.from(VERSION),
-    //     genesisHash: Buffer.from(GENESIS, 'hex')
-    // }
-
-    // const payload = PB.ClientHello.fromObject(obj)
-    // const payload = obj
-
+    /* Verify message. */
     const errMsg = PB.ClientMessage.verify(message)
     if (errMsg) {
-        debug('Protobuf verification failed!', errMsg)
+        // debug('Protobuf verification failed!', errMsg)
+        throw new Error(errMsg)
     }
 
+    /* Encode message. */
     const buffer = PB.ClientMessage.encode(message).finish()
-    debug('MESSAGE', buffer)
-    debug('MESSAGE (hex)', buffer.toString('hex'))
-    debug('MESSAGE (string)', buffer.toString())
+    debug('Encoded message:', buffer.toString('hex'))
 
-    const decoded = PB.ClientMessage.decode(buffer)
-    debug('Decoded', decoded)
-
-    // const packet = PB.encode(message)
-    // debug('PACKET', packet)
+    // const decoded = PB.ClientMessage.decode(buffer)
+    // debug('Decoded', decoded)
 
     /* Return packed message. */
-    return sendMessage(buffer)
-    // return packMessage(message)
-
-    // reply = self.recv('serverhello')
-    // self.num_components = reply.num_components
-    // self.component_feerate = reply.component_feerate
-    // self.min_excess_fee = reply.min_excess_fee
-    // self.max_excess_fee = reply.max_excess_fee
-    // self.available_tiers = tuple(reply.tiers)
-    // strong_plugin = self.strong_plugin
-    // if strong_plugin:
-    //     strong_plugin.set_remote_donation_address(reply.donation_address)
-    //
-    // # Enforce some sensible limits, in case server is crazy
-    // if self.component_feerate > 5000:
-    //     raise FusionError('excessive component feerate from server')
-    // if self.min_excess_fee > 400:
-    //     raise FusionError('excessive min excess fee from server')
+    return packMessage(buffer)
 }
 
 /**
@@ -173,91 +85,53 @@ const greet = function () {
  *
  * Encode (pack) a message into a prototype buffer (protobuf) object.
  */
-function packMessage (oneOrMorePackets) {
-    oneOrMorePackets = _.isArray(oneOrMorePackets) ? oneOrMorePackets : [oneOrMorePackets]
+function packMessage (_packets) {
+    debug('Packing message:', _packets)
 
-    /* Set packets. */
-    // const packets = PB.Packets.create({ packet: oneOrMorePackets })
+    /* Initialize message length. */
+    const messageLength = Buffer.allocUnsafe(4)
 
-    /* Set message buffer. */
-    // const messageBuffer = PB.Packets.encode(packets).finish()
-    // const messageBuffer = PB.Packets.encode(oneOrMorePackets).finish()
-    // console.log('PB', PB)
-    // const clientHello = PB.lookup('ClientHello')
-    // const messageBuffer = PB.ClientHello.encode(oneOrMorePackets).finish()
-    const messageBuffer = oneOrMorePackets
+    /* Calculate message length. */
+    const lengthDec = _packets.length
+
+    /* Set message length. */
+    // NOTE: 4-bytes in big-endian (bytes order).
+    messageLength.writeUIntBE(lengthDec, 0, 4)
+    debug('Message length:', messageLength)
+
+    /* Build packet. */
+    const packet = Buffer.concat([
+        magic,
+        messageLength,
+        _packets
+    ])
+    debug('Packet message:', packet)
+
+    return packet
+
+    // _packets = _.isArray(_packets) ? _packets : [_packets]
+
+    // const messageBuffer = _packets
 
     /* Initialize length suffix. */
-    const lengthSuffix = Buffer.alloc(4)
+    // const lengthSuffix = Buffer.alloc(4)
 
     /* Set length suffix. */
-    lengthSuffix.writeUIntBE(messageBuffer.length, 0, 4)
+    // lengthSuffix.writeUIntBE(messageBuffer.length, 0, 4)
 
     /* Set message components. */
-    const messageComponents = [magic, lengthSuffix, messageBuffer]
+    // const messageComponents = [magic, lengthSuffix, messageBuffer]
 
     /* Set full message. */
-    const fullMessage = Buffer.concat(messageComponents)
+    // const fullMessage = Buffer.concat(messageComponents)
 
     /* Return message object. */
-    return {
-        unpacked: oneOrMorePackets,
-        packed: fullMessage,
-        components: messageToBuffers(fullMessage)
-    }
+    // return {
+    //     unpacked: _packets,
+    //     packed: fullMessage,
+    //     components: messageToBuffers(fullMessage)
+    // }
 }
-
-/**
- * Message to Buffers
- */
-function messageToBuffers (someBase64Message) {
-    /* Initialize message buffer. */
-    const messageBuffer = Buffer.from(someBase64Message, 'base64')
-
-    /* Validate message buffer. */
-    if (messageBuffer.length < 12) {
-        throw new Error('bad_length')
-    } else {
-        /* Set message magic (bytes). */
-        const messageMagic = messageBuffer.slice(0, 8)
-
-        /* Validate message magic (bytes). */
-        if (messageMagic.toString('hex') !== magic.toString('hex')) {
-            throw new Error('message_magic')
-        }
-
-        /* Initialize message length. */
-        let messageLength = messageBuffer.slice(8, 12)
-
-        /* Set message length. */
-        messageLength = messageLength.readUInt32BE()
-
-        /* Set message payload. */
-        // const messagePayload = messageBuffer.slice(12, ) // FIXME: Why do we have a trailing space??
-        const messagePayload = messageBuffer.slice(12) // FIXME: Why do we have a trailing space??
-
-        /* Validate message payload. */
-        if (messagePayload.length !== messageLength) {
-            debug('Incorrect payload size:', messagePayload.length, '!==', messageLength)
-            throw new Error('message_payload')
-        } else {
-            /* Return message buffers. */
-            return {
-                magic: messageBuffer.slice(0, 8).toString('base64'),
-                length: messageBuffer.slice(8, 12).toString('base64'),
-                // payload: messageBuffer.slice(12, ).toString('base64'), // FIXME
-                payload: messageBuffer.slice(12).toString('base64'), // FIXME
-                buffer: messageBuffer.toString('base64')
-            }
-        }
-    }
-}
-
-let greeting = greet()
-// debug('Greeting', greeting.length, greeting)
-debug('Greeting', greeting.length, greeting.toString('hex'))
-
-// return
 
 /* Initialize client. */
 const client = new net.Socket()
@@ -268,12 +142,13 @@ const client = new net.Socket()
  * Opens a new connection to the host.
  */
 client.connect(PORT, HOST, function () {
-    debug('CONNECTED (host/port):', HOST, PORT)
-    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
-    // sendMessage('I am Chuck Norris!')
+    debug('Connected! (host/port):', HOST, PORT)
 
+    /* Initialize greeting. */
     const greeting = greet()
     debug('Greeting', greeting.toString('hex'))
+
+    /* Send greeting. */
     client.write(greeting)
 })
 
@@ -285,23 +160,25 @@ client.connect(PORT, HOST, function () {
 client.on('data', function (_data) {
     // debug('RECEIVED DATA:', _data)
 
-    debug('Incoming data (from server)', Buffer.from(_data).toString('hex'))
+    debug('Incoming data (from server):', Buffer.from(_data).toString('hex'))
 
     const message = _data.slice(magic.length + 4)
-    // debug('Incoming message', message.toString('hex'))
+    // debug('Incoming data (message):', message.toString('hex'))
 
     const decoded = PB.ServerMessage.decode(message)
-    debug('Incoming decoded', decoded)
+    debug('Incoming data (decoded):', decoded)
 
-    debug('Tiers', decoded.serverhello.tiers)
-    debug('# Components', decoded.serverhello.numComponents)
-    debug('Component fee rate', decoded.serverhello.componentFeerate)
-    debug('Min excess fee', decoded.serverhello.minExcessFee)
-    debug('Max excess fee', decoded.serverhello.maxExcessFee)
-    debug('Donation address', decoded.serverhello.donationAddress)
+    if (decoded.serverhello) {
+        debug('ServerHello (tiers):', decoded.serverhello.tiers)
+        debug('ServerHello (numComponents):', decoded.serverhello.numComponents)
+        debug('ServerHello (componentFeerate):', decoded.serverhello.componentFeerate)
+        debug('ServerHello (minExcessFee):', decoded.serverhello.minExcessFee)
+        debug('ServerHello (maxExcessFee):', decoded.serverhello.maxExcessFee)
+        debug('ServerHello (donationAddress):', decoded.serverhello.donationAddress)
+    }
 
     // Close the client socket completely
-    client.destroy()
+    // client.destroy()
 })
 
 /**
@@ -312,21 +189,3 @@ client.on('data', function (_data) {
 client.on('close', function () {
     debug('Connection closed.')
 })
-
-/**
- * Delay (Execution)
- */
-// const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-
-/* Initialize new wallet. */
-// const myWallet = new JsonWallet({
-//     file: './test_json_wallet.js'
-// })
-
-/* Unfreeze any frozen addresses. */
-// myWallet.unfreezeAddresses(
-//     _.map(myWallet.addresses, 'cashAddress')
-// )
-
-/* Load up our on-disk HD wallet. */
-// shuffleIt.context.wallet = myWallet
