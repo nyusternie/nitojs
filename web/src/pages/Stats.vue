0abcfb4cfc187ff6fadb5668f1afb8b80829e7a7
Shomari Prince <nyusternie@sdot.io>
2020-05-18 06:01:28 -0400

    Updated stats page.

--- web/src/pages/Stats.vue
+++ web/src/pages/Stats.vue
@@ -1,70 +1,26 @@
 <template>
     <div class="row">
         <div class="col-12">
-            <card :title="table1.title" :subTitle="table1.subTitle">
+            <card :title="pools.title" :subTitle="pools.subTitle">
                 <div slot="raw-content" class="table-responsive">
-                    <paper-table :data="table1.data" :columns="table1.columns">
-
-                    </paper-table>
+                    <paper-table
+                        :data="pools.data"
+                        :columns="pools.columns"
+                    ></paper-table>
                 </div>
             </card>
         </div>
-
-        <div class="col-12">
-            <card class="card-plain">
-                <div class="table-full-width table-responsive">
-                    <paper-table type="hover" :title="table2.title" :sub-title="table2.subTitle" :data="table2.data"
-                         :columns="table2.columns">
-
-                     </paper-table>
-                 </div>
-             </card>
-         </div>
      </div>
 </template>
 
 <script>
+/* Import modules. */
+import { BITBOX } from 'bitbox-sdk'
+import numeral from 'numeral'
+import superagent from 'superagent'
+
+/* Import components. */
 import { PaperTable } from '@/components'
-
-const tableColumns = ['Id', 'Name', 'Salary', 'Country', 'City']
-
-const tableData = [
-    {
-        id: 1,
-        name: 'Dakota Rice',
-        salary: '$36.738',
-        country: 'Niger',
-        city: 'Oud-Turnhout'
-    },
-    {
-        id: 2,
-        name: 'Minerva Hooper',
-        salary: '$23,789',
-        country: 'Curaçao',
-        city: 'Sinaai-Waas'
-    },
-    {
-        id: 3,
-        name: 'Sage Rodriguez',
-        salary: '$56,142',
-        country: 'Netherlands',
-        city: 'Baileux'
-    },
-    {
-        id: 4,
-        name: 'Philip Chaney',
-        salary: '$38,735',
-        country: 'Korea, South',
-        city: 'Overland Park'
-    },
-    {
-        id: 5,
-        name: 'Doris Greene',
-        salary: '$63,542',
-        country: 'Malawi',
-        city: 'Feldkirchen in Kärnten'
-    }
-]
 
 export default {
     components: {
@@ -72,20 +28,115 @@ export default {
     },
     data() {
         return {
-            table1: {
-                title: 'Stripped Table',
-                subTitle: 'Here is a subtitle for this table',
-                columns: [...tableColumns],
-                data: [...tableData]
-            },
-            table2: {
-                title: 'Table on Plain Background',
-                subTitle: 'Here is a subtitle for this table',
-                columns: [...tableColumns],
-                data: [...tableData]
+            bitbox: null,
+            usd: 0,
+
+            pools: {
+                title: 'CashShuffle Pool Statistics',
+                subTitle: 'loading...',
+                columns: [
+                    'Pool Id',
+                    'Minimum Satoshis',
+                    'Minimum Fiat',
+                    'Current Waiting Pool'
+                ],
+                data: [],
+                connections: 0,
             }
         }
-    }
+    },
+    computed: {
+        //
+    },
+    methods: {
+        /**
+         * Format Price
+         */
+        formatPrice: function (_satoshis) {
+            /* Set value. */
+            const value = (_satoshis / 10000000000) * this.usd
+
+            /* Return formatted value. */
+            return numeral(value).format('$0.00[00]')
+        },
+
+        /**
+         * Initialize BITBOX
+         */
+        initBitbox() {
+            console.info('Initializing BITBOX..')
+
+            try {
+                /* Initialize BITBOX. */
+                this.bitbox = new BITBOX()
+            } catch (err) {
+                console.error(err)
+            }
+        },
+
+        /**
+         * Update Price
+         */
+        async updatePrice() {
+            try {
+                /* Request current price. */
+                const current = await this.bitbox.Price.current('usd')
+                // console.log('CURRENT PRICE', current)
+
+                /* Set current price. */
+                this.usd = current
+            } catch (err) {
+                console.error(err)
+            }
+        },
+    },
+    created: async function () {
+        /* Initialize BITBOX. */
+        this.initBitbox()
+
+        /* Update USD. */
+        await this.updatePrice()
+
+        /* Set server URI. */
+        const serverUri = 'https://shuffle.servo.cash:8080/stats'
+
+        superagent
+            .get(serverUri)
+            .set('accept', 'json')
+            .end(async (err, data) => {
+                if (err) {
+                    return console.error('API ERROR:', err)
+                }
+
+                console.log('API response:', data)
+
+                /* Filter production pools. */
+                const stats = data.body.pools.filter(pool => {
+                    return pool.version === 300
+                })
+
+                /* Sort by pool tier (amount). */
+                stats.sort((a, b) => {
+                    return a.amount - b.amount
+                })
+
+                /* Set subtitle. */
+                this.pools.subTitle = `Current pool: [ shuffle.servo.cash ] has [ ${data.body.connections} ] active connections`
+
+                // console.log('Stats:', stats)
+
+                stats.forEach((pool, index) => {
+                    this.pools.data.push({
+                        poolid: `#${(index + 1)}`,
+                        minimumsatoshis: numeral(pool.amount).format('0,0'),
+                        minimumfiat: this.formatPrice(pool.amount),
+                        currentwaitingpool: `${pool.members} of 5`
+                    })
+                })
+
+            })
+
+    },
 }
 </script>
 
