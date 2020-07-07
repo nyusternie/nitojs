4c01c3d631fefc942fcc7ae1acc8366909cf115c
Shomari Prince <nyusternie@sdot.io>
2020-07-06 22:55:44 -0400

    Added 2 new blockchain crypto hashes, plus tests.

--- src/Crypto/hash.js
+++ src/Crypto/hash.js
@@ -1,4 +1,5 @@
 /* Import modules. */
+const bch = require('bitcore-lib-cash')
 const crypto = require('crypto')
 const debug = require('debug')('nitojs:crypto:hash')
 
@@ -8,14 +9,38 @@ const debug = require('debug')('nitojs:crypto:hash')
 const hash = (_message, _algo = 'sha512') => {
     debug(`Generating [ ${_algo} ] hash.`)
 
-    /* Initialize hashing algorithm. */
-    const hash = crypto.createHash(_algo)
+    /* Initialize hash hex. */
+    let hashHex = null
 
-    /* Set data. */
-    const data = hash.update(_message, 'utf-8')
+    /* Handle blockchain special-cases. */
+    if (_algo === 'sha256sha256') {
+        /* Set message buffer. */
+        const buf = Buffer.from(_message)
 
-    /* Conver to HEX format. */
-    const hashHex = data.digest('hex')
+        /* Calculate digest. */
+        const digest = bch.crypto.Hash.sha256sha256(buf)
+
+        /* Conver to hex format. */
+        hashHex = digest.toString('hex')
+    } else if (_algo === 'sha256ripemd160') {
+        /* Set message buffer. */
+        const buf = Buffer.from(_message)
+
+        /* Calculate digest. */
+        const digest = bch.crypto.Hash.sha256ripemd160(buf)
+
+        /* Conver to hex format. */
+        hashHex = digest.toString('hex')
+    } else {
+        /* Initialize hashing algorithm. */
+        const hash = crypto.createHash(_algo)
+
+        /* Set data. */
+        const data = hash.update(_message, 'utf-8')
+
+        /* Convert to hex format. */
+        hashHex = data.digest('hex')
+    }
 
     /* Return hash. */
     return hashHex
--- tests/crypto.hash.spec.js
+++ tests/crypto.hash.spec.js
@@ -23,4 +23,26 @@ describe('Crypto:hash', () => {
         /* Evaluate test. */
         expect(hash).toEqual(expected)
     })
+
+    test('it should perform a "double" SHA256 hash on a string', async () => {
+        /* Calculate hash. */
+        const hash = Nito.Crypto.hash('Hello, world', 'sha256sha256')
+
+        /* Set expected hex value. */
+        const expected = 'd9ebb60edf18fd5bbcc8f01ddb130b4122f74ccac9a1374c85b310a9a0ddddca'
+
+        /* Evaluate test. */
+        expect(hash).toEqual(expected)
+    })
+
+    test('it should perform a SHA256 and RIPEMD160 hash on a string', async () => {
+        /* Calculate hash. */
+        const hash = Nito.Crypto.hash('Hello, world', 'sha256ripemd160')
+
+        /* Set expected hex value. */
+        const expected = '02a48dfcdae3f658e5118409200b2e072029b566'
+
+        /* Evaluate test. */
+        expect(hash).toEqual(expected)
+    })
 })
