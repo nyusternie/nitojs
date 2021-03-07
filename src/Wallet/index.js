a7716da4bc4a2e2e632138b0103e0a5ace0e83ef
nyusternie <nyusternie@sdot.io>
2021-03-07 11:27:05 -0500

    Add method to create new wallet account.

--- src/Wallet/index.js
+++ src/Wallet/index.js
@@ -8,18 +8,63 @@ const EventEmitter = require('events').EventEmitter
  * High level functions for simple crypto transactions.
  */
 class Wallet extends EventEmitter {
-    constructor(_seed, _derivationPath) {
+    constructor(_params) {
         super()
 
-        /* Validate derivation path. */
-        if (_derivationPath) {
-            this.derivationPath = _derivationPath
+        /* Initialize mnemonic. */
+        this._mnemonic = null
+
+        /* Initialize 32-byte seed. */
+        this._seed = null
+
+        /* Initialize wallet key. */
+        this._walletKey = null
+
+        /* Initialize derivation path. */
+        this._derivationPath = null
+
+        /* Initialize security level. */
+        this._securityLevel = null
+
+        /* Validate wallet key. */
+        if (_params.key) {
+            this._walletKey = _params.key
         } else {
-            // NOTE: Hardened account path.
-            this.derivationPath = `m/44'/145'/0'`
+            this._walletKey = _params
         }
 
-        debug(`Wallet class has been initialized by with [ ${_seed} ] at [ ${_derivationPath} ]`)
+        /* Validate derivation path. */
+        if (_params.path) {
+            this._derivationPath = _params.path
+        } else {
+            // NOTE: Hardened account path.
+            this._derivationPath = `m/44'/145'/0'`
+        }
+
+        /* Validate security. */
+        if (_params.security) {
+            this._securityLevel = _params.security
+        } else {
+            this._securityLevel = 24 // NOTE: 12 = 128-bit, 24 = 256-bit
+        }
+
+        debug(`Wallet class has been initialized by with [ ${this._walletKey} ] at [ ${this._derivationPath} ] with [ ${this.security} ]`)
+
+        /* Initialize accounts. */
+        this._accounts = []
+
+        /* Initialize (default) account. */
+        this._accounts[0] = this.createAccount()
+    }
+
+    /* Accounts */
+    get accounts() {
+        return this._accounts
+    }
+
+    /* Create Account */
+    createAccount(_params) {
+        return require('./createAccount').bind(this)(_params)
     }
 
 }
